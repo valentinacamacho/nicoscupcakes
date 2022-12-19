09ecaed565befae61f05d6e7cb57b386feaf1072
valentinacamacho <valentinacamacho0206@gmail.com>
2022-12-19 14:40:20 -0500

    Se inicializo creando  arquitectura de carpetas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Layout/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>prueba</h1>
+
+ 
     </div>
   );
 }
