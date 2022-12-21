45f00b92108a808b819519dca9ffd0fc421d0318
valentinacamacho <valentinacamacho0206@gmail.com>
2022-12-21 10:11:20 -0500

    En el componente header se crea estructura de como van a ir los elementos

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,2 +1,28 @@
 import React from "react";
 
+function Header(){
+  return(
+
+    <header className="contenedor-principal-header">
+    <div className="header">
+
+    <img className="LogoCup"
+    src={LogoCup}
+    alt='Nico´s Cupcakes'/>
+
+    <div className="carrito-arriba">
+      <div className="circulo1">
+        <div className="circulo2-contador">  
+        </div>
+      </div>
+    </div>
+
+    <img className="Icono-menu"
+    src={iconmenu}
+    alt='Icono'/>   
+    </div>    
+    </header>
+  );
+}
+
+export default Header;
