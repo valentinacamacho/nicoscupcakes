51d7a7dc6a3a9c1dbd27a3dd63c85c1a5022c370
valentinacamacho <valentinacamacho0206@gmail.com>
2022-12-22 16:47:39 -0500

    En la vista secundaria se coloco el avatar y se coloco propiedades y valores para centralizar los elementos

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
 
-    {/* <Home/> */}
+    {/* { <Home/> } */}
 
-    <VistaSecun/>
+    {<VistaSecun/> }
     
     </div>
   );
--- src/Pages/VistaSecun.js
+++ src/Pages/VistaSecun.js
@@ -10,21 +10,25 @@ function VistaSecun (){
 
         <Header/>
 
-        <div className='Recuadro-Vistasecun'>
-          <div className='avatar-'>
-            <img 
-              src={avatar}
-              alt='avatar'
-            />
+
+          <div className='Recuadro-Vistasecun'>
+            <div className='avatar'>
+              <img 
+                src={avatar}
+                alt='avatar'/>
+            <div className='boton-vistasecun'>
+              <button >Conoce Nuestros Productos</button>
+            </div>
           </div>
+          
 
         <div className='texto1-secun'>
-          <h1 className='titulo-secun'>!Quienes somos¡</h1>
+          <h1 className='titulo-secun'>¡Quienes somos!</h1>
           <p>En Nico’s Cupcakes creamos deliciosos dulces y mini reposteria, con exquisitas recetas y sabores originales; hechos con ingredientes de alta calidad.</p>
           <p>Ofrecemos deliciosos Cupcakes, Mini tortas y paletas de pastel</p>
           <p>Somos un emprendimiento artesanal donde dedicamos el tiempo necesario a las necesidades del cliente</p>
         </div>      
-        </div>
+      </div>
 
         < Footer
           texto1='Derechos reservados-Nico´s Cupcakes'
